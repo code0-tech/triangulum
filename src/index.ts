c160c0217fe8cb570e138a65d1300055ec95bcb0
nicosammito <nico@sammito.de>
2026-03-02 06:53:11 +0100

    feat: create index file to export validation and suggestion modules

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './data';
+export * from './getFlowValidation';
+export * from './getNodeSuggestions';
+export * from './getNodeValidation';
+export * from './getReferenceSuggestions';
+export * from './getTypeFromValue';
+export * from './getValueFromType';
+export * from './getValueSuggestions';
+export * from './getValueValidation';
+export * from './utils';
+
