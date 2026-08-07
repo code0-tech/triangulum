4292dfc0fcfec880efa968077581573ed0a2de77
nicosammito <nsammito@code0.tech>
2026-08-07 16:12:17 +0200

    feat: export getFlowSchemas from extraction module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './extraction/getTypeFromValue';
 export * from './extraction/getValueFromType';
+export * from './extraction/getFlowSchemas';
 export * from './validation/getFlowValidation';
 export * from './validation/getValueValidation';
 export * from './schema/getSignatureSchema';
