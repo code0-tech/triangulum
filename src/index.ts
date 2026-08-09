4c56d6532f71272247ab178281887de19fa28898
nicosammito <nsammito@code0.tech>
2026-08-09 14:25:52 +0200

    feat: add schema utility exports to index for improved accessibility

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './extraction/getTypeFromValue';
 export * from './extraction/getValueFromType';
+export * from './util/schema.util';
 export * from './validation/getFlowValidation';
 export * from './validation/getValueValidation';
 export * from './schema/getSignatureSchema';
